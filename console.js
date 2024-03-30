5cc2c36326fccad50bd879b82eb11710646dc078
Mubarak Abdullahi <120288041+4mubarak@users.noreply.github.com>
2024-03-30 17:00:18 +0100

    Create console.js

---
+++ console.js
@@ -0,0 +1,17 @@
+// This is a JavaScript file named console.js
+
+// Function to print a greeting message to the console
+function greet(name) {
+    console.log("Hello, " + name + "!");
+}
+
+// Function to calculate the square of a number
+function square(num) {
+    return num * num;
+}
+
+// Example usage of the greet function
+greet("John");
+
+// Example usage of the square function
+console.log("Square of 5:", square(5));
